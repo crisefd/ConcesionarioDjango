b540b841b8b586fb4585269573ca94264d92186a
crisefd <cristhian.fuertes@correounivalle.edu.co>
2015-10-03 14:14:14 -0500

    se agregan validaciones al login

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -12,13 +12,13 @@ class LoginView(SuccessMessageMixin, FormView):
     template_name = 'login.html'
     success_url = '/'
     def form_valid(self, form):
-        print "validando formulario"
+        self.success_url = ''
         user = authenticate(username = form.cleaned_data['username'], 
                     password = form.cleaned_data['password'])
         if user is not None:
-            print "el usuario ", user.username, "existe"
+            #print "el usuario ", user.username, "existe"
             if user.is_active:
-                print "el usuario esta activo"
+                #print "el usuario esta activo"
                 if user.charge == "Gerente":
                     self.success_url += "/cuentas/gerente/" + user.username
                 elif user.charge == "Vendedor":
@@ -27,19 +27,33 @@ class LoginView(SuccessMessageMixin, FormView):
                     self.success_url += "cuentas/jefetaller/" + user.username
                 messages.add_message(self.request, messages.SUCCESS, "Bienvenido " + user.username)
                 login(self.request, user)
+            else:
+                self.success_url += '/login/'
+                messages.add_message(self.request, messages.ERROR, "El usuario" + user.username + " no esta activo")
+        else:
+            self.success_url += '/login/'
+            messages.add_message(self.request, messages.ERROR, "El usuario" + no existe)
+
         return super(LoginView, self).form_valid(form)
 
+    def form_invalid(self, form):
+        messages.add_message(self.request, messages.ERROR, "Formulario incorrecto, revise nombre de usuario y contrasena")
+        return super(LoginView, self).form_invalid(form)        
 
-class RegisterView(FormView):
+
+class RegisterView(SuccessMessageMixin, FormView):
     form_class = MyUserCreationForm
     template_name = "registro_usuario.html"
     succes_url = "/registrar/"
 
     def form_valid(self, form):
         form.save()
-        messages.add_message(self.request, messages.SUCCESS, "Se ha registrado exitosamente al usuario " + user.username)
+        messages.add_message(self.request, messages.SUCCESS, "Se ha registrado exitosamente al usuario ")
         return super(RegisterView, self).form_valid(form)
 
+    def form_invalid(self, form):
+        messages.add_message(self.request, messages.ERROR, "No se pudo registrar al usuario")
+        return super(RegisterView, self).form_invalid(form)
 
 
 def inicio_gerente(request):
