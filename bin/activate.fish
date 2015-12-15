b974691b0abbf43c4a93c316649238b7d2d64d6c
crisefd <cristhian.fuertes@correounivalle.edu.co>
2015-12-15 13:50:13 -0500

    se intenta arregalr mensajes

--- bin/activate.fish
+++ bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/crisefd/djcode/proyecto_www"
+set -gx VIRTUAL_ENV "/home/crixx93/djcode/proyecto_www"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
