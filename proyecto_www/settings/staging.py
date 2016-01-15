5084e01b94193c653efc9421c32e236fdce394b7
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-01-15 18:58:35 -0500

    segundo commit

--- proyecto_www/settings/staging.py
+++ proyecto_www/settings/staging.py
@@ -23,7 +23,7 @@ DATABASES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = (BASE_DIR,'static',)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=(BASE_DIR,'static',)
