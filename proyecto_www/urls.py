4b939e7da6b1f2f4f32c7aaa77817c4f946968c6
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-01-16 14:22:17 -0500

    ASDF

--- proyecto_www/urls.py
+++ proyecto_www/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.conf import settings
 
 
 urlpatterns=[
