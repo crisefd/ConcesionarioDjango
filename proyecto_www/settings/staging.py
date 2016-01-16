38f3907ac9d9b61c11edd83f4b7f34f0250b8d1f
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-01-16 14:18:15 -0500

    sdfasd

--- proyecto_www/settings/staging.py
+++ proyecto_www/settings/staging.py
@@ -8,14 +8,13 @@ TEMPLATE_DEBUG = True
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-   
      'default':{
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME' : 'concesionario',
     'USER' : 'postgres',
     'PASSWORD' : 'postgres',
     'HOST' : 'localhost',
-    'PORT' : '5432'
+    'PORT' : '5432',
     }
 }
 
