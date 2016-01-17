623e35c89eee6f0536f22763731f9c49e00f9cd3
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-01-17 16:36:09 -0500

    :)

--- proyecto_www/wsgi.py
+++ proyecto_www/wsgi.py
@@ -18,7 +18,7 @@ import os
 #application = get_wsgi_application()
 
 #For deployment
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto_www.settings.staging")
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(get_wsgi_application())
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto_www.settings.staging")
+#from django.core.wsgi import get_wsgi_application
+#from whitenoise.django import DjangoWhiteNoise
+#application = DjangoWhiteNoise(get_wsgi_application())
