495c5dc1ab3a47d89b8d6539a05292939e7594ab
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-02-15 10:32:49 -0500

    commit

--- proyecto_www/wsgi.py
+++ proyecto_www/wsgi.py
@@ -14,14 +14,14 @@ import os
 
 
 #For deployment
-#try:
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto_www.settings.staging")
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(get_wsgi_application())
-#except:
+try:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto_www.settings.staging")
+    from django.core.wsgi import get_wsgi_application
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(get_wsgi_application())
+except:
 #For development
-#   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto_www.settings.local")
-#    from django.core.wsgi import get_wsgi_application
-#    application = get_wsgi_application()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto_www.settings.local")
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
 
