89231c36e4998d6aa807ab50b30dfee9d84f046a
crisefd <cristhian.fuertes@correounivalle.edu.co>
2016-02-15 15:31:00 -0500

    mm

--- proyecto_www/settings/staging.py
+++ proyecto_www/settings/staging.py
@@ -10,10 +10,10 @@ TEMPLATE_DEBUG = True
 DATABASES = {
      'default':{
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME' : 'd5uvpg5dj9g35b',
-    'USER' : 'ckodvufbczcflz',
-    'PASSWORD' : '6myNkz2Fx7ifEWFriz073wqGhY',
-    'HOST' : 'ec2-54-83-61-45.compute-1.amazonaws.com',
+    'NAME' : 'de2o9j0emnosgn',
+    'USER' : 'kjuerftixrfohi',
+    'PASSWORD' : 'UNdItjNXG60Kdn9TZgE0uzt3Fw',
+    'HOST' : 'ec2-54-83-29-133.compute-1.amazonaws.com',
     'PORT' : '5432',
     }
 
